6492b3c0b47bd50195b9fa0c36af387f607b8ea6
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-24 03:27:25 +0700

    Update landing.js

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -11,4 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     btn.textContent = isOpen ? "xem thêm" : "thu gọn";
   });
 });
- 
+ // mobile menu
+const menuToggle = document.getElementById("menuToggle");
+const menuOverlay = document.getElementById("menuOverlay");
+const menuClose = document.getElementById("menuClose");
+
+if (menuToggle && menuOverlay) {
+  menuToggle.addEventListener("click", () => {
+    menuOverlay.classList.add("active");
+  });
+}
+
+if (menuClose) {
+  menuClose.addEventListener("click", () => {
+    menuOverlay.classList.remove("active");
+  });
+}
+
