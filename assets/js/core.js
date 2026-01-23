8b1662e9346e10d87c3bcfcabc04246243609c95
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-23 15:49:25 +0700

    Update core.js

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,1 +1,11 @@
+const menuToggle = document.getElementById("menuToggle");
+const mobileMenu = document.getElementById("mobileMenu");
+const closeMenu = document.getElementById("closeMenu");
 
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+});
