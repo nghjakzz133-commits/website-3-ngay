2f9a3fc00a901e3edc91f242de9064812ee3f1bc
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-24 01:35:19 +0700

    Update landing.js

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -11,29 +11,4 @@ document.addEventListener("DOMContentLoaded", () => {
     btn.textContent = isOpen ? "xem thêm" : "thu gọn";
   });
 });
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleBtn = document.getElementById('menuToggle');
-  const overlay = document.getElementById('menuOverlay');
-  const closeBtn = document.getElementById('menuClose');
-
-  if (!toggleBtn || !overlay) return;
-
-  // mở menu
-  toggleBtn.addEventListener('click', () => {
-    overlay.classList.add('active');
-  });
-
-  // đóng menu khi click overlay
-  overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
-      overlay.classList.remove('active');
-    }
-  });
-
-  // đóng menu bằng nút X
-  if (closeBtn) {
-    closeBtn.addEventListener('click', () => {
-      overlay.classList.remove('active');
-    });
-  }
-});
+ 
