ee6e7032b755271d60157c791bb1d0b2762e0bf9
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-26 16:18:46 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
+// Toggle menu mobile
+const navToggle = document.getElementById('navToggle');
+const navMenu = document.getElementById('navMenu');
 
+if (navToggle && navMenu) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+}
